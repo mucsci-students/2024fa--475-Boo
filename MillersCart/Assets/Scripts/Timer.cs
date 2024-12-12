7c9b2f8e091017740fafb7bf1505cb86d6daffb0
tyreynolds611 <152023789+tyreynolds611@users.noreply.github.com>
2024-12-11 20:40:51 -0500

    Timer

---
+++ MillersCart/Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        timerText.text = elapsedTime.ToString();
+    }
+
+}
